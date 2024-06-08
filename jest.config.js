fe4fd41e393d7dca54f01840908f0e769e6ec214
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2024-06-08 15:00:37 +0300

    feat(test-report): add reporters for test execution

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  reporters: [
+    'default',
+    ['jest-junit', {outputDirectory: 'reports', outputName: 'report.xml'}],
+    ['github-actions', {silent: false}],
+    'summary'
+  ],
 }
